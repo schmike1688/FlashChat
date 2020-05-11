8f894223770a38d83e4dfe6289e0b7b17a8f47e2
Naomi <naomi@Naomis-iMac.local>
2020-05-10 22:55:21 -0700

    Complete Flash Chat

---
+++ Flash Chat/NewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewTableViewCell.swift
+//  Flash Chat
+//
+//  Created by Naomi on 9/5/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
